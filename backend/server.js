2708aaacc8dd4f93ac30f875474926a9976e6a41
Kausyar-coder <Kausyarsh@gmail.com>
2026-01-22 19:20:02 +0100

    Week 1: Step 4 GET routes completed

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,49 @@
-import cors from "cors"
-import express from "express"
+import cors from "cors";
+import express from "express";
+import data from "./data.json";
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
-})
+  res.json({
+    name: "Happy Thoughts API",
+    endpoints: [
+      { method: "GET", path: "/", description: "API docs" },
+      { method: "GET", path: "/thoughts", description: "Get all thoughts" },
+      {
+        method: "GET",
+        path: "/thoughts/:id",
+        description: "Get single thought by id",
+      },
+    ],
+  });
+});
+
+app.get("/thoughts", (req, res) => {
+  res.send(data.thoughts);
+});
+
+app.get("/thoughts/:id", (req, res) => {
+  const id = req.params.id;
+
+  const thought = data.thought.find((t) => t._id === id);
+
+  if (!thought) {
+    return res.status(404).json({
+      message: "not found",
+      id,
+    });
+  }
+
+  res.json(thought);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
